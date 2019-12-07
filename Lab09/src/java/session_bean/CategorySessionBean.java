6336bfaf2fab5897ddf0b5bb7da834314c7b4f01
long-vh0599 <57719239+long-vh0599@users.noreply.github.com>
2019-12-07 23:21:54 +0700

    Add files via upload

---
+++ Lab09/src/java/session_bean/CategorySessionBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session_bean;
+
+import entity.Category;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author Admin
+ */
+public class CategorySessionBean extends AbstractSessionBean<Category>{
+    @PersistenceContext(unitName = "eMarketCU")
+    private EntityManager em;
+    
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    public CategorySessionBean() {
+        super(Category.class);
+    }
+}
---
+++ Lab09/src/java/session_bean/ProductSessionBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session_bean;
+
+import entity.Product;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Admin
+ */
+@Stateless
+public class ProductSessionBean extends AbstractSessionBean<Product> {
+    @PersistenceContext(unitName = "eMarketPu")
+    private EntityManager em;
+    
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    public ProductSessionBean() {
+        super(Product.class);
+    }
+}
